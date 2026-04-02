1ca6f9686cc8e245d4b9aad760074cf3f7ad270c
Emily H <157163774+emheyboer@users.noreply.github.com>
2026-04-01 23:11:15 -0400

    the forest update
    press '>' and '<' to zoom in and out. i'm not sure what scale is appropriate, so this is mostly for testing that. also the long-term plan here is to adopt a data-driven approach to sprites so sprites.py doesn't become such a hot mess

--- src/mapScreen.py
+++ src/mapScreen.py
@@ -1,17 +1,37 @@
+import pygame
+
 from screen import Screen
+from sprites import Sprite
 
 class MapScreen(Screen):
+    def __init__(self, screen, width: int, height: int, sprites: dict[str, Sprite], player, level: int) -> None:
+        super().__init__(screen, width, height, sprites, player, level)
+        self.size = 32
+
     def draw(self) -> None:
         """
         Renders the screen
         """
-        for x in range(0, 18):
-            for y in range(0, 11):
-                sprite = self.sprites[f"map_{x:02d}_{y:02d}"]
-                sprite.draw(self.screen, (x * 16, y * 16), (16, 16))
+        size = self.size
+        columns = self.width // size
+        rows = self.height // size
+        for x in range(0, columns):
+            for y in range(0, rows):
+                self.sprites[f"map_01_00"].draw(self.screen,
+                                                (x * size, y * size), (size, size))
+                self.sprites[f"map_04_06"].draw(self.screen,
+                                                (x * size, y * size), (size, size))
     
     def on_event(self, event):
         """
         Responds to an individual pygame event. On screen change, returns the new screen
         """
+
+        if event.type == pygame.KEYUP:
+            if event.unicode == '>':
+                self.size <<= 1
+            elif event.unicode == '<':
+                self.size >>= 1
+            print(self.size)
+
         return self
